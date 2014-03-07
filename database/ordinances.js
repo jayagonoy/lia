e75b0f16a05ad3f9b8ad5a240b7464f04ebcc179
Jay Agonoy <tcmanila@gmail.com>
2014-03-07 08:45:13 +0000

    major rehaul made from earlier pushed into the oblivion(?)

---
+++ database/ordinances.js
@@ -0,0 +1,9 @@
+var ord1="This is a test sample of code. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+var ord2="This is a test sample of code. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+var ord3="This is a test sample of code. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
+with (document) {
+	write('<p>' + ord1 + '</p>');
+	write('<p>' + ord2 + '</p>');
+	write('<p>' + ord3 + '</p>');
+}
