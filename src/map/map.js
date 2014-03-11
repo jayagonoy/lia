02d03eddc607929cc4261da9c0c2c7d8b4d9b4e3
Jay Agonoy <tcmanila@gmail.com>
2014-03-11 14:43:16 +0000

    added map.js

---
+++ src/map/map.js
@@ -0,0 +1,41 @@
+﻿
+	var cities = new L.LayerGroup();
+
+	var cmAttr = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
+		cmUrl = 'http://{s}.tile.cloudmade.com/10567f6272c647fea571f0d96235b618/{styleId}/256/{z}/{x}/{y}.png';
+
+        var minimal   = L.tileLayer(cmUrl, {styleId: 22677, attribution: cmAttr}),		
+	    midnight  = L.tileLayer(cmUrl, {styleId: 999,   attribution: cmAttr}),
+	    motorways = L.tileLayer(cmUrl, {styleId: 46561, attribution: cmAttr});
+
+	var map = L.map('map', {
+		center: [14.60223, -238.89354],
+		zoom: 15,
+		layers: [minimal, motorways, cities]
+	});
+
+	var baseLayers = {
+		"Minimal": minimal,
+		"Night View": midnight
+	};
+
+	var overlays = {
+		"Motorways": motorways,
+		"Cities": cities
+	};
+
+	L.control.layers(baseLayers, overlays).addTo(map);
+	L.control.scale().addTo(map);
+	
+
+// Tool to help plotting
+	
+	var popup = L.popup();
+	function onMapClick(e) {
+		popup
+			.setLatLng(e.latlng)
+			.setContent(e.latlng.toString())
+			.openOn(map);
+	}
+	map.on('click', onMapClick);
+
