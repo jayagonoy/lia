5a69800ac6d23ef41b681cef296cc7f4613c5f52
Jay Agonoy <tcmanila@gmail.com>
2014-03-12 12:50:26 +0000

    Changes listed in the CHANGELOG as of MArch 12, 3:12AM

---
+++ database/plot.js
@@ -0,0 +1,5 @@
+// Plot all the ordinances fetched from database/ordinances.js in the map 
+	
+	L.marker([14.57694, 121.11483]).addTo(map).bindPopup(ord1);
+	L.marker([14.5894, 121.12049]).addTo(map).bindPopup(ord2);
+	L.marker([14.61149, 121.12015]).addTo(map).bindPopup(ord3);
