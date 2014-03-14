7481e1ec932036fdad6473aa9a502c3f374f6403
Jay Agonoy <tcmanila@gmail.com>
2014-03-14 13:39:24 +0000

    new edits as of pi day, 3.14.2014 - more on the changelog.

--- database/plot.js
+++ database/plot.js
@@ -1,5 +1,6 @@
 // Plot all the ordinances fetched from database/ordinances.js in the map 
 	
-	L.marker([14.57694, 121.11483]).addTo(map).bindPopup(ord1);
-	L.marker([14.5894, 121.12049]).addTo(map).bindPopup(ord2);
-	L.marker([14.61149, 121.12015]).addTo(map).bindPopup(ord3);
+	L.marker([14.58072, 121.1156]).addTo(map).bindPopup(ord2012003);
+	L.marker([14.58136, 121.11574]).addTo(map).bindPopup(ord2012001);
+	L.marker([14.58549, 121.11538]).addTo(map).bindPopup(ord2008009);
+	
