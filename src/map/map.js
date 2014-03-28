8c6a9f542952ca5be0448b81a57b5b9fdb881d6c
Jay Agonoy <tcmanila@gmail.com>
2014-03-28 11:34:24 -0400

    OSM tile replacement failed, tries MapBox

--- src/map/map.js
+++ src/map/map.js
@@ -1,14 +1,11 @@
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+var mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v3/assortex.hl2c1pgo/{z}/{x}/{y}.png', {
+    attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
+});
+var map = L.map('map')
+    .addLayer(mapboxTiles)
+    .setView([14.58292, 121.11757], 15);
 
-	var map = L.map('map', {
-		center: [14.58292, 121.11757],
-		zoom: 15,
-	});
-
-	L.control.scale().addTo(map);
-	
+L.control.scale().addTo(map);
 
 // Tool to help plotting
 	
