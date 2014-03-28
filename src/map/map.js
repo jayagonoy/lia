81a7451dbf71e75f772b827b69e3870ee88e2427
Jay Agonoy <tcmanila@gmail.com>
2014-03-28 11:07:59 -0400

    replaced Cloudmade with OSM tiles

--- src/map/map.js
+++ src/map/map.js
@@ -1,30 +1,12 @@
-﻿
-	var cities = new L.LayerGroup();
-
-	var cmAttr = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
-		cmUrl = 'http://{s}.tile.cloudmade.com/10567f6272c647fea571f0d96235b618/{styleId}/256/{z}/{x}/{y}.png';
-
-        var minimal   = L.tileLayer(cmUrl, {styleId: 22677, attribution: cmAttr}),		
-	    midnight  = L.tileLayer(cmUrl, {styleId: 999,   attribution: cmAttr}),
-	    motorways = L.tileLayer(cmUrl, {styleId: 46561, attribution: cmAttr});
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
 	var map = L.map('map', {
 		center: [14.58292, 121.11757],
 		zoom: 15,
-		layers: [minimal, motorways, cities]
 	});
 
-	var baseLayers = {
-		"Minimal": minimal,
-		"Night View": midnight
-	};
-
-	var overlays = {
-		"Motorways": motorways,
-		"Cities": cities
-	};
-
-	L.control.layers(baseLayers, overlays).addTo(map);
 	L.control.scale().addTo(map);
 	
 
